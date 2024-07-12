5927a8d055670d532296b71398c5b07290d9528f
MSI\hajii <mr.haciisma@gmail.com>
2024-07-12 04:19:09 +0400

    small updates

---
+++ lib/models/post.dart
@@ -0,0 +1,13 @@
+class Post {
+  String imageUrl;
+  String title;
+  String authors;
+  int views;
+
+  Post({
+    required this.imageUrl,
+    required this.title,
+    required this.authors,
+    required this.views,
+  });
+}
---
+++ lib/widgets/post_cart.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class PostCard extends StatelessWidget {
+  const PostCard({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.authors,
+    required this.views,
+  });
+
+  final String imageUrl;
+  final String title;
+  final String authors;
+  final int views;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 240,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        elevation: 5,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Image.network(
+                imageUrl,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    authors,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  Text(
+                    'Views: $views',
+                    style: TextStyle(
+                      fontSize: 10,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
